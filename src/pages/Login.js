ab889a4182de26d3ac772c2ebceb889670b43324
Marinavdac <marinavdac@gmail.com>
2022-06-29 14:27:12 -0300

    fix: corrige identação lint

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,43 +3,43 @@ import PropTypes from 'prop-types';
 import LoginForm from '../components/LoginForm';
 
 class Login extends Component {
-  state = {
-    name: '',
-    email: '',
-    isDisabled: true,
-  }
+state = {
+  name: '',
+  email: '',
+  isDisabled: true,
+}
 
-  handleChange = ({ target }) => {
-    const { name, value } = target;
-    this.setState({ [name]: value }, this.handleButton);
-  };
+handleChange = ({ target }) => {
+  const { name, value } = target;
+  this.setState({ [name]: value }, this.handleButton);
+};
 
-  handleButton = () => {
-    const { name, email } = this.state;
-    if (name.length !== 0 && email.length !== 0) {
-      return this.setState({ isDisabled: false });
-    }
-    return this.setState({ isDisabled: true });
+handleButton = () => {
+  const { name, email } = this.state;
+  if (name.length !== 0 && email.length !== 0) {
+    return this.setState({ isDisabled: false });
   }
+  return this.setState({ isDisabled: true });
+}
 
-  handleConfig = () => {
-    const { history } = this.props;
-    history.push('/settings');
-  }
+handleConfig = () => {
+  const { history } = this.props;
+  history.push('/settings');
+}
 
-  render() {
-    const { isDisabled } = this.state;
-    const { handleChange, handleConfig } = this;
-    const { history } = this.props;
-    return (
-      <LoginForm
-        handleChange={ handleChange }
-        handleConfig={ handleConfig }
-        isDisabled={ isDisabled }
-        history={ history }
-      />
-    );
-  }
+render() {
+  const { isDisabled } = this.state;
+  const { handleChange, handleConfig } = this;
+  const { history } = this.props;
+  return (
+    <LoginForm
+      handleChange={ handleChange }
+      handleConfig={ handleConfig }
+      isDisabled={ isDisabled }
+      history={ history }
+    />
+  );
+}
 }
 
 Login.propTypes = {

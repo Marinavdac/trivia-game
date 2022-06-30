0fd870f30e8c0e4c51c2b600fb6bc3330564423d
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-06-30 15:42:04 -0300

    Requisito 18

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -3,7 +3,15 @@ import React, { Component } from 'react';
 class Ranking extends Component {
   render() {
     return (
-      <h1 data-testid="ranking-title">Ranking</h1>
+      <>
+        <button
+          type="button"
+          data-testeid="btn-go-home"
+        >
+          Volte ao início
+        </button>
+        <h1 data-testid="ranking-title">Ranking</h1>
+      </>
     );
   }
 }

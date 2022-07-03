50835550b02f0f1aef2296f3f7a17f2ae22afda7
Marinavdac <marinavdac@gmail.com>
2022-07-03 16:20:28 -0300

    continua e modifica requisito 8: timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -4,37 +4,68 @@ import PropTypes from 'prop-types';
 class Timer extends Component {
 state = {
   timer: 30,
+  timerId: '',
 }
 
-shouldComponentUpdate = (nextProps, nextState) => {
-  const { timer } = this.state;
-  const { parentCallBack } = this.props;
-  if (nextState.timer !== timer) {
-    parentCallBack(timer);
-    return true;
+// shouldComponentUpdate = (nextProps, nextState) => {
+//   const { timer } = this.state;
+//   const { parentCallBack } = this.props;
+//   if (nextState.timer !== timer) {
+//     parentCallBack(timer);
+//     return true;
+//   }
+// }
+
+// componentDidMount = () => {
+//   const sec = 1000;
+//   setInterval(() => {
+//     this.setState((prevState) => ({
+//       timer: prevState.timer > 0 ? (prevState.timer - 1) : 'timeout',
+//     }));
+//   }, sec);
+// }
+
+// https://stackoverflow.com/questions/36299174/setinterval-in-a-react-app
+
+componentDidMount() {
+  const sec = 1000;
+  const timerId = setInterval(this.timeCounter, sec);
+  this.setState({
+    timerId,
+  });
+}
+
+componentWillUnmount() {
+  const { timerId } = this.state;
+  clearInterval(timerId);
+}
+
+  timeCounter = () => {
+    const { parentCallBack } = this.props;
+    const { timer, timerId } = this.state;
+    const currentTime = timer - 1;
+    if (currentTime >= 0) {
+      this.setState({
+        timer: currentTime,
+      });
+    } else {
+      clearInterval(timerId);
+    }
+    if (currentTime === 0) {
+      parentCallBack(currentTime, true);
+    }
+  }
+
+  render() {
+    const { timer } = this.state;
+    return (
+      <h1>{ timer }</h1>
+    );
   }
 }
 
-componentDidMount = () => {
-  const sec = 1000;
-  setInterval(() => {
-    this.setState((prevState) => ({
-      timer: prevState.timer > 0 ? (prevState.timer - 1) : 'Time Out',
-    }));
-  }, sec);
-}
-
-render() {
-  const { getTimer } = this.props;
-  const { timer } = this.state;
-  return (
-    <h1 timer={ getTimer }>{ timer }</h1>
-  );
-}
-}
-
 Timer.propTypes = {
-  getTimer: PropTypes.func.isRequired,
+  // getTimer: PropTypes.func.isRequired,
   parentCallBack: PropTypes.func.isRequired,
 };
 
--- src/services/fetchGravatar.js
+++ src/services/fetchGravatar.js
@@ -3,7 +3,6 @@ import md5 from 'crypto-js/md5';
 const getAvatar = (email) => {
   const gravatarImage = md5(email).toString();
   const ENDPOINT = `https://www.gravatar.com/avatar/${gravatarImage}`;
-  console.log(email);
   return ENDPOINT;
 };
 

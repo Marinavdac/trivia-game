c99dca0112dcdadc6f2068de3332b4e3695271e7
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-07-07 09:04:38 -0300

    Requisito 19 finalizado

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,24 +1,61 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import getAvatar from '../services/fetchGravatar';
 
 class Ranking extends Component {
+  state = {
+    rankingState: null,
+  };
+
   btnHome = () => {
     const { history } = this.props;
     history.push('/');
-  }
+  };
+
+  componentDidMount = () => {
+    const { score, email, name } = this.props;
+    const gravatarPic = getAvatar(email);
+    const array = {
+      name,
+      score,
+      email: gravatarPic,
+    };
+    localStorage.setItem('player', JSON.stringify(array));
+    const ranking = JSON.parse(localStorage.getItem('ranking'));
+    const teste = JSON.parse(localStorage.getItem('player'));
+    if (ranking === null) {
+      const juntatudo = [teste];
+      localStorage.setItem('ranking', JSON.stringify(juntatudo));
+      const estado = JSON.parse(localStorage.getItem('ranking'));
+      this.setState({ rankingState: estado });
+    } else {
+      const juntatudo = [...ranking, teste];
+      localStorage.setItem('ranking', JSON.stringify(juntatudo));
+      const estado = JSON.parse(localStorage.getItem('ranking'));
+      const esse = estado.sort(function (a, b) {
+        return b.score - a.score;
+      });
+      this.setState({ rankingState: estado });
+    }
+  };
 
   render() {
+    const { rankingState } = this.state;
     return (
-      <div className="trivia-game-screen">
-        <button
-          type="button"
-          data-testid="btn-go-home"
-          onClick={ this.btnHome }
-        >
+      <>
+        <button type="button" data-testid="btn-go-home" onClick={ this.btnHome }>
           Volte ao início
         </button>
         <h1 data-testid="ranking-title">Ranking</h1>
-      </div>
+        {rankingState?.map(({ email, score, name }, index) => (
+          <div key={ index }>
+            <img src={ email } alt="gravatar" />
+            <p data-testid={`player-name-${ index }`}>{ name }</p>
+            <p data-testid={`player-score-${ index }`}>{ score }</p>
+          </div>
+        ))}
+      </>
     );
   }
 }
@@ -27,6 +64,15 @@ Ranking.propTypes = {
   history: PropTypes.shape({
     push: PropTypes.func.isRequired,
   }).isRequired,
+  name: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
+  email: PropTypes.string.isRequired,
 };
 
-export default Ranking;
+const mapStateToProps = (state) => ({
+  email: state.player.gravatarEmail,
+  name: state.player.name,
+  score: state.player.score,
+});
+
+export default connect(mapStateToProps)(Ranking);
